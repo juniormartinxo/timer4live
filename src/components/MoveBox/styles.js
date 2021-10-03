a17f07c00fe96986ad43643a3721adc201e0c15e
Junior Martins <amjr.box@gmail.com>
2021-10-03 11:58:26 -0300

    Mudança do cursor para grab no estado normal e grabbing quando ativo, para dar a impressão que está pegando o timer e carregando ele

--- src/components/MoveBox/styles.js
+++ src/components/MoveBox/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 
 const Box = styled.div`
-  cursor: pointer;
+  cursor: grab;
   position: absolute;
   z-index: 9;
   text-align: center;
+
+  &:active {
+    cursor: grabbing;
+  }
 `
 export default Box
